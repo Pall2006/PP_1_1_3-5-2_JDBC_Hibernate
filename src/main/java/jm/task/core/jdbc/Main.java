144b6fdf918cd813c270dcad94daee345a5d5c43
Martianov Pavel <pall2006@bk.ru>
2023-04-12 11:46:19 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl user = new UserServiceImpl();
+        user.createUsersTable();
+        user.saveUser("Иван", "Петров", (byte) 21);
+        user.saveUser("Ирина", "Сидорова", (byte) 25);
+        user.saveUser("Николай", "Иванов", (byte) 30);
+        user.saveUser("Евгений", "Пименов", (byte) 24);
+        user.getAllUsers();
+        user.cleanUsersTable();
+        user.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static String URL = "jdbc:mysql://localhost:3306/katamysql";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "mart0580";
+
+    // Создаем метод который возвращает Connection
+      public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection is OK");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR");
+        }
+        return connection;
+
+    }
+
+
 }
